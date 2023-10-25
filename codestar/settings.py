256d1924bb1186cd936384de386c97feffa474c5
Emma Cadavra <emmacadavra@gmail.com>
2023-10-25 15:18:55 +0000

    Teaks before deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['therefore-i-blog-fsf.herokuapp.com', '8000-emmacadavra-thereforeib-toi93kwnmhy.ws-eu105.gitpod.io', 'therefore-i-blog-fsf-bb7c614be2ab.herokuapp.com']
 
